d899c901820949ead31ec734c6e58240bb849396
Garreth - Smartz <garrethv@smartz-solutions.com>
2023-11-05 11:53:04 +0200

    Initial Push

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig, loadEnv } from "vite";
+//import plugins from "./plugins";
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        server: {
+            port: 3000, // To run the app on port 3000
+            open: true // If we want to open the app once its started
+        },
+    };
+});
